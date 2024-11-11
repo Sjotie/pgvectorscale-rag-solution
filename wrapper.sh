9162893b5094867d3848ce4d962f4abf1753b49e
Sjotie <124035684+Sjotie@users.noreply.github.com>
2024-11-11 10:48:12 +0100

    Update wrapper.sh

--- wrapper.sh
+++ wrapper.sh
@@ -4,8 +4,5 @@ set -e
 unset PGHOST
 unset PGPORT
 
-if [[ "$LOG_TO_STDOUT" == "true" ]]; then
-    /usr/local/bin/docker-entrypoint.sh "$@" 2>&1
-else
-    /usr/local/bin/docker-entrypoint.sh "$@"
-fi
+# Use the correct path to docker-entrypoint.sh
+exec /usr/local/bin/docker-entrypoint.sh "$@"
